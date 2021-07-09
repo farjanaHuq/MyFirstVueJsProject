8f267bf945e3c394509aa967f1737b672cc48771
Farjana Huq <fa.huq03@gmail.com>
2021-07-09 11:28:28 -0700

    created first page

---
+++ app.js
@@ -0,0 +1,23 @@
+// import Vue from 'vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// // Import Bootstrap an BootstrapVue CSS files (order is important)
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// // Make BootstrapVue available throughout your project
+// Vue.use(BootstrapVue)
+// // Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin)
+
+// import Vue from 'vue'
+// import { BootstrapVue } from 'bootstrap-vue'
+
+// import './style.scss'
+
+// Vue.use(BootstrapVue)
+
+const app = Vue.createApp({
+    template: '<h1>My First Vue Project</h1>',
+})
+app.mount('#app')
