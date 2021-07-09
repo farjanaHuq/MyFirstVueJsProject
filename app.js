c88c2b1362356cdada30080007cf7e7bdede82ab
Farjana Huq <fa.huq03@gmail.com>
2021-07-09 13:29:18 -0700

    project complete

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-// import Vue from 'vue'
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-// // Import Bootstrap an BootstrapVue CSS files (order is important)
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-// // Make BootstrapVue available throughout your project
-// Vue.use(BootstrapVue)
-// // Optionally install the BootstrapVue icon components plugin
-// Vue.use(IconsPlugin)
-
-// import Vue from 'vue'
-// import { BootstrapVue } from 'bootstrap-vue'
-
-// import './style.scss'
-
-// Vue.use(BootstrapVue)
-
 const app = Vue.createApp({
-    template: '<h1>My First Vue Project</h1>',
-})
-app.mount('#app')
+    data() {
+      return {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@gmail.com',
+        gender: 'male',
+        picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+      }
+    },
+    methods: {
+      async getUser() {
+        const res = await fetch('https://randomuser.me/api')
+        const { results } = await res.json()
+  
+        // console.log(results)
+  
+        this.firstName = results[0].name.first
+        this.lastName = results[0].name.last
+        this.email = results[0].email
+        this.gender = results[0].gender
+        this.picture = results[0].picture.large
+      },
+    },
+  })
+  
+  app.mount('#app')
+  
